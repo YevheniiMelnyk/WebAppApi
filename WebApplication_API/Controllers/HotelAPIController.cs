ba4cdf5436de6234b2606dce4b7a53b3b35c6d28
YevheniiMelnyk <melnik.zheka@gmail.com>
2023-07-11 15:55:49 +0300

    initial commit with two hotels

---
+++ WebApplication_API/Controllers/HotelAPIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication_API.Model;
+
+namespace WebApplication_API.Controllers
+{
+    [Route("api/HotelAPI")]
+    [ApiController]
+    public class HotelAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Hotel> GetHotels()
+        {
+            return new List<Hotel> 
+            {
+                new Hotel { Id = 1, Name = "Menalo Hotel Premium"}, 
+                new Hotel { Id = 2, Name = "The Green Park Ankara"}
+            };
+        }
+    }
+}
---
+++ WebApplication_API/Models/Hotel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication_API.Model
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
